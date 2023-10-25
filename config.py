b9fb353693a6a750569a87ee2fedac140a8d2709
sohag02 <70505164+sohag02@users.noreply.github.com>
2023-10-25 15:04:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 class Config:
 
-    API_ID = 1927002
-    API_HASH = "a5b3aa12e55b8183353a1039ccd109e1"
-    BOT_TOKEN = "1902248091:AAGRqdT-6OlM0r5XcZrwL-BvZaFut_ekncY"
-    PLAYER_API_ID = API_ID#7310466
-    PLAYER_API_HASH = API_HASH#"b419c4ba5652ce8606356fd55efdd11e"
-    CHAT_ID = -1001385556080, -1001591369347, -1001296119865
+    # Add Your own config here
+    API_ID = 
+    API_HASH = 
+    BOT_TOKEN = 
+    PLAYER_API_ID = 
+    PLAYER_API_HASH = 
+    CHAT_ID = 
     RADIO_URL = "https://radioindia.net/radio/hungamanow/icecast.audio"
